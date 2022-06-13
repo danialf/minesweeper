44b01e3e225699f2dbbcd7143594084e3b5a2812
Danial Farzaneh <danialf@live.com>
2022-06-13 15:03:42 +0300

    open field functionaity

--- ms/src/board.rs
+++ ms/src/board.rs
@@ -24,7 +24,16 @@ impl Board {
         }
     }
 
-    fn iter_neighbors<'a>(&'a self, (rows, cols): Position) -> impl Iterator<Item = Position> + 'a {
+    fn get(&self, position: Position) -> Field {
+        self[position.0][position.1]
+    }
+
+    fn set(&mut self, position: Position, value: Field) {
+        self[position.0][position.1] = value;
+    }
+
+    /// iterate over neighboring elements by given position
+    fn iter_neighbors(&self, (rows, cols): Position) -> impl Iterator<Item = Position> + '_ {
         ((rows.max(1) - 1)..=(rows + 1).min(self.get_size().get_rows() - 1))
             .flat_map(move |i| {
                 ((cols.max(1) - 1)..=(cols + 1).min(self.get_size().get_cols() - 1))
@@ -32,6 +41,102 @@ impl Board {
             })
             .filter(move |&pos| pos != (rows, cols))
     }
+
+    /// count number of the mines adjacent to given position
+    fn count_neighboring_mines(&self, position: Position) -> u8 {
+        self.iter_neighbors(position)
+            .filter(|elem| match self.get(*elem) {
+                Field::Mine(_) => true,
+                _ => false,
+            })
+            .count() as u8
+    }
+
+    /// count number of the flagged field adjacent to given position
+    fn count_neighboring_flags(&self, position: Position) -> u8 {
+        self.iter_neighbors(position)
+            .filter(|&position| match self.get(position) {
+                Field::Mine(state) => match state {
+                    MineState::Hidden => false,
+                    MineState::Detonated => false,
+                    MineState::Flagged => true,
+                },
+                Field::Flagged => true,
+                _ => false,
+            })
+            .count() as u8
+    }
+
+    /// toggle flag given position
+    fn toggle_flag(&mut self, position: Position) {
+        match self.get(position) {
+            Field::Closed => self.set(position, Field::Flagged),
+            Field::Opened(_) => return,
+            Field::Mine(state) => match state {
+                MineState::Hidden => self.set(position, Field::Mine(MineState::Flagged)),
+                MineState::Detonated => return,
+                MineState::Flagged => self.set(position, Field::Mine(MineState::Hidden)),
+            },
+            Field::Flagged => self.set(position, Field::Closed),
+        }
+    }
+
+    /// .open a field by given position
+    pub fn open(&mut self, position: Position) {
+        match self.get(position) {
+            Field::Closed => {
+                // open neighboring too
+                let mine_count = self.count_neighboring_mines(position);
+
+                if mine_count == 0 {
+                    self.set(position, Field::Opened(0));
+
+                    let neighbors: Vec<Position> = self.iter_neighbors(position).collect();
+                    for neighbor in neighbors {
+                        if self.get(neighbor) == Field::Closed {
+                            self.open(neighbor);
+                        }
+                    }
+                } else {
+                    self.set(position, Field::Opened(mine_count));
+                }
+            }
+            Field::Opened(mine_count) => {
+                // marked all possible mines?
+                let flag_count = self.count_neighboring_flags(position);
+
+                if mine_count == flag_count {
+                    let neighbors: Vec<Position> = self
+                        .iter_neighbors(position)
+                        .filter(|&pos| match self.get(pos) {
+                            Field::Closed => true,
+                            Field::Opened(_) => false,
+                            Field::Mine(state) => match state {
+                                MineState::Hidden => true,
+                                MineState::Detonated => true,
+                                MineState::Flagged => false,
+                            },
+                            Field::Flagged => false,
+                        })
+                        .collect();
+
+                    for pos in neighbors {
+                        self.open(pos);
+                    }
+                }
+                return;
+            }
+            Field::Mine(state) => match state {
+                MineState::Hidden => self.set(position, Field::Mine(MineState::Detonated)),
+                MineState::Detonated => return,
+                MineState::Flagged => return,
+            },
+            Field::Flagged => return,
+        }
+    }
+
+    /// reveal all the mins
+    pub fn reveal(&self) {}
 
     pub fn reset(&mut self) {
         for i in 0..self.fields.len() {
@@ -102,6 +207,26 @@ mod tests {
     }
 
     #[test]
+    fn test_board_open() {
+        let mut b = Board::new(10, 10);
+
+        // b[0][0] = Field::Opened(3);
+        b[1][0] = Field::Mine(MineState::Hidden);
+        b[0][1] = Field::Mine(MineState::Hidden);
+        b[1][1] = Field::Mine(MineState::Hidden);
+
+        b.open((0, 0));
+
+        println!("{}", b);
+
+        b.open((2, 1));
+        println!("{}", b);
+
+        b.open((3, 3));
+        println!("{}", b);
+    }
+
+    #[test]
     #[ignore]
     fn test_board_display() {
         let mut b = Board::new(10, 10);
@@ -109,6 +234,7 @@ mod tests {
         b[0][0] = Field::Opened(3);
         b[1][0] = Field::Flagged;
         b[0][1] = Field::Mine(MineState::Hidden);
+        b[0][2] = Field::Mine(MineState::Hidden);
 
         b[9][9] = Field::Mine(MineState::Detonated);
         println!("{}", b);
@@ -147,30 +273,28 @@ mod tests {
     }
 
     #[test]
-    fn test_board_iter_neighbors_upper_bound()
-    {
-        let b = Board::new(5,5);
-        
-        let neighbors: Vec<Position> = b.iter_neighbors((0,0)).collect();
+    fn test_board_iter_neighbors_upper_bound() {
+        let b = Board::new(5, 5);
 
-        assert_eq!(neighbors.len(),3);
+        let neighbors: Vec<Position> = b.iter_neighbors((0, 0)).collect();
 
-        assert_eq!(neighbors.contains(&(1,0)),true);
-        assert_eq!(neighbors.contains(&(1,1)),true);
-        assert_eq!(neighbors.contains(&(0,1)),true);
+        assert_eq!(neighbors.len(), 3);
+
+        assert_eq!(neighbors.contains(&(1, 0)), true);
+        assert_eq!(neighbors.contains(&(1, 1)), true);
+        assert_eq!(neighbors.contains(&(0, 1)), true);
     }
 
     #[test]
-    fn test_board_iter_neighbors_lower_bound()
-    {
-        let b = Board::new(5,5);
-        
-        let neighbors: Vec<Position> = b.iter_neighbors((4,4)).collect();
+    fn test_board_iter_neighbors_lower_bound() {
+        let b = Board::new(5, 5);
 
-        assert_eq!(neighbors.len(),3);
+        let neighbors: Vec<Position> = b.iter_neighbors((4, 4)).collect();
 
-        assert_eq!(neighbors.contains(&(4,3)),true);
-        assert_eq!(neighbors.contains(&(3,3)),true);
-        assert_eq!(neighbors.contains(&(3,4)),true);
+        assert_eq!(neighbors.len(), 3);
+
+        assert_eq!(neighbors.contains(&(4, 3)), true);
+        assert_eq!(neighbors.contains(&(3, 3)), true);
+        assert_eq!(neighbors.contains(&(3, 4)), true);
     }
 }
